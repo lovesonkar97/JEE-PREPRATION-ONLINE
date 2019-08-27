0b8db9f8909869e2a3a6c6623d634c6641ab06ec
lovesonkar97 <lovesonkar029@gmail.com>
2019-08-27 22:10:42 +0530

    Jee Preparation | Online

---
+++ script.js
@@ -0,0 +1,73 @@
+$(()=>{
+    // physics
+    $('#physics1-wpe').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PLYVDsiuOZP5osjR-_wk4eepEWaLVMCJu1'))
+})
+
+$('#physics1-nl').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PLYVDsiuOZP5qh0ApJ4qjKnIVYLTV_judA'))
+})
+
+$('#physics1-cm').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PLYVDsiuOZP5oDvG83OvDW1-MUBXRMZ7Wb'))
+})
+
+$('#physics2-1').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PLJZk2__oyAljX55R_1RZ2mqb3B_dTqz2g'))
+})
+
+$('#physics2-2').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PLJZk2__oyAlgeXYwIryfWxl8fP2hU5orq'))
+})
+
+// chemistry
+$('#chemistry-ec').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PLCtPeXfq-7Qjhj3niRdgJXUX3Boz3ou1i'))
+})
+
+$('#chemistry-s').click(()=>{$('#main-r').empty();
+$('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PLCtPeXfq-7Qjzp9h4-s2HB1nKFgWaoyUE'))
+})
+
+$('#chem-soa').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PLCtPeXfq-7QhJ1yXkxzlXFpuWuMxJDuf8'))
+})
+
+$('#chem-ss').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PLCtPeXfq-7QhCDGpyKNvAdKN9iX9pVct8'))
+})
+
+$('#chem2-oc').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PL7rZUH8srnuOMBjl6ZE6V6PRfb_dmyxTF'))
+})
+
+$('#chem2-hc').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PL7rZUH8srnuMrpLXNW17WNT1Iqio7I9Ii'))
+})
+
+
+$('#chem2-as').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PL7rZUH8srnuPDq0qqll-zDvjKYsFVhMYv'))
+})
+
+
+// maths
+
+$('#maths-f').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PL_A4M5IAkMad5zB0Dh6gUw1eYK8dN7hP7'))
+})
+
+$('#maths-bt').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PL_A4M5IAkMacSgRRlEkUB9v-gE1yxw_rG'))
+})
+
+$('#maths-qe').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PL_A4M5IAkMaf5Ga3nQJe-gg-0zXG77YRB'))
+})
+
+
+$('#maths-p').click(()=>{$('#main-r').empty();
+    $('#main-r').append($('<iframe>').css('width','100%').css('height','100%').attr('src','https://www.youtube.com/embed/videoseries?list=PL_A4M5IAkMaeT3qAAgcvUXiKnM044FO44'))
+})
+
+})
